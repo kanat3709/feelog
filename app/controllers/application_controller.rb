f6566d9576542dcbc351505a2714d69f3dd4f29a
kanat3709 <tec.sa.lmnt@gmail.com>
2026-04-23 20:01:58 +0900

    会員登録機能とログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :terms_of_service])
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessor :terms_of_service
+
+  validates :name, presence: true
+  validates :terms_of_service, acceptance: true
 end
