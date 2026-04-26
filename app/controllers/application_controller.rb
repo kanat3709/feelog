93a09add4ed8b49d32592a151d54ccc1a84a6acb
kanat3709 <tec.sa.lmnt@gmail.com>
2026-04-26 21:24:03 +0900

    fix: Rubocopの指摘を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :terms_of_service])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name terms_of_service])
   end
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     root_path
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     root_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     root_path
   end
 end
