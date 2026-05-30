7ac7d1faf79061a9b29c67cad9c5a4ad0f705006
kanat3709 <tec.sa.lmnt@gmail.com>
2026-05-30 18:11:45 +0900

    管理者による公開投稿の非表示・再表示機能を追加

--- app/controllers/public_posts_controller.rb
+++ app/controllers/public_posts_controller.rb
@@ -5,7 +5,7 @@ class PublicPostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
-    @posts = Post.where(is_public: true)
+    @posts = Post.where(is_public: true, hidden: false)
                  .includes(:user)
                  .order(created_at: :desc)
   end
@@ -17,5 +17,6 @@ class PublicPostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
     raise ActiveRecord::RecordNotFound unless @post.is_public?
+    raise ActiveRecord::RecordNotFound if @post.hidden? && !current_user&.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_30_073839) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_30_083214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_30_073839) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.boolean "hidden", default: false, null: false
     t.boolean "is_public", default: false, null: false
     t.decimal "latitude", precision: 10, scale: 6
     t.decimal "longitude", precision: 10, scale: 6
