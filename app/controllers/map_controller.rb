cea9be2cc7bc18d7ce2fa4d07b24a28b91e5a61c
kanat3709 <tec.sa.lmnt@gmail.com>
2026-05-31 12:30:46 +0900

    マップ表示機能を実装（Google Maps API連携）(#46)

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class MapController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
+
+  def index
+    @posts = current_user.posts.where.not(latitude: nil, longitude: nil)
+  end
 end
