18fccb7a7c034ef2ac9031cfa9f39f74f55f8b14
kanat3709 <tec.sa.lmnt@gmail.com>
2026-06-01 13:04:15 +0900

    RuboCop修正：map_controllerのwhere.not記法と末尾改行を修正

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,6 +4,6 @@ class MapController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts.where.not(latitude: nil, longitude: nil)
+    @posts = current_user.posts.where('latitude IS NOT NULL AND longitude IS NOT NULL')
   end
 end
