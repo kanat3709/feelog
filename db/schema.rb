d16cd938c1f86cd0ecbf2f0c6075f0728a7a786d
kanat3709 <tec.sa.lmnt@gmail.com>
2026-06-17 18:38:48 +0900

    行きたい！機能と一覧ページを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_13_020640) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_17_065531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -104,6 +104,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_13_020640) do
     t.bigint "post_id", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.boolean "visited", default: false, null: false
     t.index ["post_id"], name: "index_wish_lists_on_post_id"
     t.index ["user_id", "post_id"], name: "index_wish_lists_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_wish_lists_on_user_id"
